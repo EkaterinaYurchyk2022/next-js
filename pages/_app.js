ce425352e422a1cb3e32074df55ff3d42bd7a57a
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2023-02-16 23:18:32 +0300

    Pages & routes, Layout & components, styles, pages navigation, images & metadata, fetching data

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
+import Layout from "@/components/Layout";
+import Image from "next/image";
+import nextImage from '../public/next.svg'
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+
+const App = ({Component, pageProps}) => (
+    <Layout>
+        <Head >
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet"/>
+        </Head>
+        <main>
+            <Component {...pageProps} />
+        </main>
+        <Image
+            src={nextImage}
+            blurDataURL={'../public/next.svg'}
+            width={500}
+            height={350}
+            alt='preview'
+            placeholder='blur'
+/>
+    </Layout>
+
+)
+
+export default App
+
+
+
